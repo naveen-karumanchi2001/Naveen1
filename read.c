2b1c45bebccaa10dec9cbd915b3a2842a6507682
naveen-karumanchi2001 <naveen.karumanchi@truminds.com>
2025-04-24 16:09:36 +0530

    Update read.c

--- read.c
+++ read.c
@@ -9,25 +9,30 @@ int main() {
     int shmid = shmget(SHM_KEY, sizeof(SharedData), 0666);
 
     SharedData *data = (SharedData *)shmat(shmid, NULL, 0);
-    
     int semid = semget(SEM_KEY, 1, 0666);
- 
     struct sembuf lock = {0, -1, 0};
+ 
     struct sembuf unlock = {0, 1, 0};
+ 
+    // Lock semaphore before accessing shared memory
     semop(semid, &lock, 1);
+ 
+    // Read the message from shared memory
     char received[MAX_MESSAGE_LEN];
     strncpy(received, data->message, MAX_MESSAGE_LEN);
+ 
+    // Unlock semaphore after reading
     semop(semid, &unlock, 1);
-
+ 
+    // Print received message
     printf("Received: %s\n", received);
  
-    FILE *ptr = fopen("shmem.txt", "a+");
+    // Log to the shared log file
+    FILE *ptr = fopen("shmem.txt", "a");
     fprintf(ptr, "Process ID: %d: %s\n", getpid(), received);
     fclose(ptr);
 
     shmdt(data);
-    shmctl(shmid,IPC_RMID,NULL);
-    semctl(semid,0,IPC_RMID);
  
     return 0;
 }
